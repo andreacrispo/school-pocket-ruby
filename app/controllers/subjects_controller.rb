90bd66bc23d6c329253b109bbb7210654e0da60d
Andrea <andrea.crispo.cr@gmail.com>
2015-08-09 23:11:50 +0200

    Sistemata navbar

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,16 +1,21 @@
 class SubjectsController < ApplicationController
   
   before_action :authenticate_user
-
+  
+  # get /subjects
   def index
+    @subjects = Subject.all
   end
 
   def show
   end
 
+  # get /subjects/new 
   def new
+    # render subject form
   end
-
+  
+  # post /subjects
   def create
   	
   end
