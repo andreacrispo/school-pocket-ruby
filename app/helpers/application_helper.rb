33091890d342dc96db54e8c4585bb9dfb5481aac
Andrea <andrea.crispo.cr@gmail.com>
2015-08-13 01:18:17 +0200

    Fixed page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
 
-	def title
-	  base_title = "School Pocket"
-	  if base_title
-	  	base_title
-	  else
-	  	"#{page_title} | #{base_title}"
-	  end
-	end
+  def full_title
+	base_title = "School Pocket"
+	"#{base_title} | #{ controller_path.capitalize}"
+  end
+
+
 end
